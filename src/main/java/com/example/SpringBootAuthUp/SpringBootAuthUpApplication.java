be93f70ddb000253460fbc1a5a579b775e64950f
NingYue915 <1819841827@qq.com>
2020-09-02 09:24:06 +0800

    commit

---
+++ src/main/java/com/example/SpringBootAuthUp/SpringBootAuthUpApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootAuthUp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAuthUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAuthUpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringBootAuthUp/SpringBootAuthUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootAuthUp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAuthUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
